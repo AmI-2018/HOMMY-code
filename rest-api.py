7b520f79439b355fc3be33ebba5fd4cab4a3ce15
lore-lml <lorenzo.limoli@studenti.polito.it>
2018-05-25 20:56:26 +0200

    test smartphone - server interno

--- rest-api.py
+++ rest-api.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request
-import requests
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+import requests, webbrowser
 app = Flask(__name__)
-ONLINE_SERVER = "127.0.0.1:5000";
+ONLINE_SERVER = "http://127.0.0.1:5000"
+THIS_SERVER = "http://192.168.1.111:5000"
 
 @app.route('/')
 def hello_world():
@@ -9,9 +10,14 @@ def hello_world():
 
 
 @app.route('/categories')
-def index():
-    result = requests.get(ONLINE_SERVER + "/categories")
-    return render_template('categories.html', res = result)
+def categories():
+    r = requests.get(ONLINE_SERVER + "/categories")
+    return render_template('categories.html', res = r.json())
+
+@app.route('/categoriesM')
+def categoriesM():
+    webbrowser.open(THIS_SERVER + "/categories")
+    return "Ciao"
 
 if __name__ == '__main__':
     app.run()
